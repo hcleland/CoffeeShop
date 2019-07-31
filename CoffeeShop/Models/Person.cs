6619cafe51650c65afa1f10f8c9d08ed37254e92
Heather Cleland <heathercleland@gmail.com>
2019-07-31 16:40:08 -0500

    first commit

---
+++ CoffeeShop/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person Parent { get; set; }
+    }
+}
